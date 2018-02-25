1f94e39347de59a1de2d021c0832f72bcf57a02d
Matthew Morgan <matt@dulcetsoftware.com>
2018-02-24 20:29:15 -0500

    Green solution for acronym

---
+++ acronym/acronym.go
@@ -0,0 +1,22 @@
+// Package acronym converts a phrase to an acronym
+package acronym
+
+import (
+  "strings"
+  "regexp"
+)
+
+// take a string s and return an acronym
+func Abbreviate(s string) string {
+  acronym := ""
+  for _, token := range tokenize(s) {
+    acronym += strings.ToUpper(string(token[0]))
+  }
+  return acronym
+}
+
+func tokenize(phrase string) []string {
+  nocommas := regexp.MustCompile(`[,]`).ReplaceAllString(phrase, "")
+  nodashes := regexp.MustCompile(`[-]`).ReplaceAllString(nocommas, " ")
+  return strings.Split(nodashes, " ")
+}
