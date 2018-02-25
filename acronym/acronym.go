b40230d1d7af5be4df59c127c5d3253932d221f1
Matthew Morgan <matt@dulcetsoftware.com>
2018-02-25 08:04:25 -0500

    Apply gofmt

--- acronym/acronym.go
+++ acronym/acronym.go
@@ -2,21 +2,21 @@
 package acronym
 
 import (
-  "strings"
-  "regexp"
+	"regexp"
+	"strings"
 )
 
 // take a string s and return an acronym
 func Abbreviate(s string) string {
-  acronym := ""
-  for _, token := range tokenize(s) {
-    acronym += strings.ToUpper(string(token[0]))
-  }
-  return acronym
+	acronym := ""
+	for _, token := range tokenize(s) {
+		acronym += strings.ToUpper(string(token[0]))
+	}
+	return acronym
 }
 
 func tokenize(phrase string) []string {
-  nocommas := regexp.MustCompile(`[,]`).ReplaceAllString(phrase, "")
-  nodashes := regexp.MustCompile(`[-]`).ReplaceAllString(nocommas, " ")
-  return strings.Split(nodashes, " ")
+	nocommas := regexp.MustCompile(`[,]`).ReplaceAllString(phrase, "")
+	nodashes := regexp.MustCompile(`[-]`).ReplaceAllString(nocommas, " ")
+	return strings.Split(nodashes, " ")
 }
