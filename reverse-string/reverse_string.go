1c399522edfef568c699499cdd2cad7c8dbc266e
Matthew Morgan <matt@dulcetsoftware.com>
2019-04-14 14:18:50 -0400

    Add solution for reverse-string

---
+++ reverse-string/reverse_string.go
@@ -0,0 +1,9 @@
+// Package reverse has a method that reverses a string
+package reverse
+
+import "github.com/golang/example/stringutil"
+
+// method String takes a string as inpput and returns it reversed
+func String(input string) string {
+	return stringutil.Reverse(input)
+}
