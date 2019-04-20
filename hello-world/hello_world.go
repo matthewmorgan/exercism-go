5c3e13c85d5adc4b1b12e91f75c781226af04d37
Matthew Morgan <matthewmorgan@Matthews-MacBook-Pro.local>
2019-04-20 15:51:51 -0400

    Add soln for hello-world

---
+++ hello-world/hello_world.go
@@ -0,0 +1,7 @@
+// Package greeting has one function that says 'Hello, World!'
+package greeting
+
+// HelloWorld takes no arguments and returns a string.
+func HelloWorld() string {
+	return "Hello, World!"
+}
