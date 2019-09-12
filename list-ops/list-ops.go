aac118e4218982afa17a9d396c887663dca07e75
Matthew Morgan <matthewmorgan@Matthews-MacBook-Pro.local>
2019-09-12 06:30:24 -0400

    Pass foldr

--- list-ops/list-ops.go
+++ list-ops/list-ops.go
@@ -15,8 +15,8 @@ func (list IntList) Foldl(op binFunc, initial int) int {
 
 func (list IntList) Foldr(op binFunc, initial int) int {
 	acc := initial
-	for _, el := range list {
-		acc = op(acc, el)
+	for i := len(list); i > 0 ; i-- {
+		acc = op(list[i-1], acc)
 	}
 	return acc
 }
