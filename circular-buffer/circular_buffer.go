62103005355d83b4879f8ab02c32ff3420e08a03
Matthew Morgan <matthewmorgan@Matthews-MacBook-Pro.local>
2019-09-29 21:00:21 -0400

    Pass initial test - read fail on empty buffer

---
+++ circular-buffer/circular_buffer.go
@@ -0,0 +1,34 @@
+package circular
+
+import "errors"
+
+type Buffer struct {
+	slots []byte
+	maxSize int
+}
+
+func NewBuffer(size int) *Buffer {
+	var b = Buffer {[]byte{}, size}
+	return &b
+}
+
+func (b *Buffer) ReadByte() (byte, error) {
+	if len(b.slots) == 0 {
+		return '0', errors.New("read failed")
+	}
+	var result = b.slots[0]
+	b.slots = b.slots[1:]
+	return result, nil
+}
+
+func (b *Buffer) WriteByte(c byte) error {
+	return nil
+}
+
+func (b *Buffer) Overwrite(c byte) {
+
+}
+
+func (b *Buffer) Reset() {
+
+}
