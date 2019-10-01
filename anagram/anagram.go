d72ca12b434efa088aec6d182d288bc207024031
Matthew Morgan <matthewmorgan@Matthews-MacBook-Pro.local>
2019-10-01 06:27:07 -0700

    Better method name

--- anagram/anagram.go
+++ anagram/anagram.go
@@ -11,11 +11,11 @@ type maybeAnagram struct {
 }
 
 func Detect(subject string, candidates []string) []string {
-	var parsedSubject = stringToSortedString(subject)
+	var parsedSubject = stringToMaybeAnagram(subject)
 	var anagrams = []string{}
 
 	for _, candidate := range candidates {
-		var parsedCandidate = stringToSortedString(candidate)
+		var parsedCandidate = stringToMaybeAnagram(candidate)
 		if parsedCandidate.isAnagramOf(&parsedSubject) {
 			anagrams = append(anagrams, candidate)
 		}
@@ -27,7 +27,7 @@ func (m1 *maybeAnagram) isAnagramOf(m2 *maybeAnagram) bool {
 	return m1.sorted == m2.sorted && m1.lowercase != m2.lowercase
 }
 
-func stringToSortedString(input string) maybeAnagram {
+func stringToMaybeAnagram(input string) maybeAnagram {
 	var lower = strings.ToLower(input)
 	var lettersArray = strings.Split(lower, "")
 	sort.Strings(lettersArray)
