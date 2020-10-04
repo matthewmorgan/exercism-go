d5bd1a0bd5220d1caf243070fdb05600caf984fa
Matthew Morgan <matt@dulcetsoftware.com>
2020-10-04 09:23:41 -0400

    Commit working result for parallel letter frequency

--- parallel-letter-frequency/frequency.go
+++ parallel-letter-frequency/frequency.go
@@ -13,23 +13,31 @@ func Frequency(s string) FreqMap {
 	return m
 }
 
-// ConcurrentFrequency counts the frequency of each rune in a given text and returns this
-// data as a FreqMap.
-func ConcurrentFrequency(languageStrings []string) FreqMap {
-	mapsChannel := make(chan FreqMap)
-	for _, languageString := range languageStrings {
-		// Using a lambda function here.  Not sure I like this better than using a named function.
-		go func(s string, c chan FreqMap) {
-			c <- Frequency(s)
-		}(languageString, mapsChannel)
+// ConcurrentFrequency calculates the letter frequency for an array of strings.
+// Each string in the array represents a different language.
+func ConcurrentFrequency(strings []string) FreqMap {
+	// Create a map to aggregate counts by letter across all languages.
+	sum := FreqMap{}
+	// count is number of strings AKA languages.
+	count := len(strings)
+	// Create a channel to queue computed results for a given language.
+	results := make(chan FreqMap, count)
+
+	// Start a goroutine to calculate the frequency map for each lanugage,
+	// sending the results to our channel AKA queue.
+	for _, s := range strings {
+		go func(s string) {
+			results <- Frequency(s)
+		}(s)
 	}
 
-	allLanguageCounts := FreqMap{}
-	for range languageStrings {
-		languageMap := <- mapsChannel
-		for r, count := range languageMap {
-			allLanguageCounts[r] += count
+	// Aggregate the frequency maps passed back via the results channel,
+	// summing counts by letter across all languages.
+	for i := 0; i < count; i++ {
+		for r, freq := range <-results {
+			sum[r] += freq
 		}
 	}
-	return allLanguageCounts
+
+	return sum
 }
